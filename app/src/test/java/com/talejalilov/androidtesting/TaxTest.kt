1ec2d2f1d30dd97d0f29f5d7c8bf3fafa854dd2d
talejalilov <talejalilov@gmail.com>
2022-04-15 18:56:28 +0300

    Base Testing

---
+++ app/src/test/java/com/talejalilov/androidtesting/TaxTest.kt
@@ -0,0 +1,18 @@
+package com.talejalilov.androidtesting
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class TaxTest {
+
+    @Test
+    fun calculateTax() {
+
+
+    }
+
+    @Test
+    fun calculateIncome() {
+    }
+}
